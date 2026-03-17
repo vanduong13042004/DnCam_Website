29cf5f4bb613f872fc77f1f69a19ba67fe767a00
Nguyễn Văn Dương <nguyenvanduong130404@gmail.com>
2026-03-17 09:28:27 +0700

    thêm file cá nhân vào gitignore

--- dn_cam_frontend/src/components/Header.jsx
+++ dn_cam_frontend/src/components/Header.jsx
@@ -17,9 +17,7 @@ const Header = () => {
           <Link to="/introduce" className="hover:text-blue-600 transition-colors">Giới thiệu</Link>
         </nav>
 
-        {/* Khu vực Tìm kiếm & Icon */}
         <div className="flex items-center gap-6">
-          {/* Ô tìm kiếm */}
           <div className="relative hidden lg:block">
             <input 
               type="text" 
@@ -29,7 +27,6 @@ const Header = () => {
             <Search className="w-4 h-4 text-gray-400 absolute left-4 top-1/2 -translate-y-1/2" />
           </div>
 
-          {/* Giỏ hàng có số lượng */}
           <Link to="/cart" className="relative text-gray-600 hover:text-blue-600 transition-colors">
             <ShoppingCart className="w-6 h-6" />
             <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
@@ -37,7 +34,6 @@ const Header = () => {
             </span>
           </Link>
           
-          {/* Nút Đăng nhập / User */}
           <Link to="/login" className="text-gray-600 hover:text-blue-600 transition-colors">
             <User className="w-6 h-6" />
           </Link>
