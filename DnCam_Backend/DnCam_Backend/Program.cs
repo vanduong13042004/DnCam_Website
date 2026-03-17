d9ef03403c995bc6083ce1399f221062a67c752e
Nguyễn Văn Dương <nguyenvanduong130404@gmail.com>
2026-03-17 09:29:47 +0700

    kết nối db

--- DnCam_Backend/DnCam_Backend/Program.cs
+++ DnCam_Backend/DnCam_Backend/Program.cs
@@ -1,20 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DnCam_Backend.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// --- 1. ĐĂNG KÝ CÁC DỊCH VỤ (SERVICES) ---
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Đăng ký DbContext để kết nối SQL Server
+builder.Services.AddDbContext<DnCamContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Cấu hình CORS (Để web React ở cổng 5173 có thể gọi được API này)
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", policy => {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// --- 2. CẤU HÌNH LUỒNG XỬ LÝ (MIDDLEWARE) ---
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Kích hoạt CORS (Phải đặt trước UseAuthorization)
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
